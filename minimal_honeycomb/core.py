d6ecd55408cf4298f3a420581a18b89e66e8e5ce
Theodore Quinn <tcquinn@tcquinn.org>
2019-11-29 11:44:34 -0800

    Implement compound requests

--- minimal_honeycomb/core.py
+++ minimal_honeycomb/core.py
@@ -1,5 +1,4 @@
 from gqlpycgen.client import Client, FileUpload
-from gqlpycgen.api import QueryBase, MutationBase
 from uuid import uuid4
 import json
 import os
@@ -44,8 +43,6 @@ class MinimalHoneycombClient:
                 'client_secret': client_secret,
             }
         )
-        self.mutation = MutationBase(self.client)
-        self.query = QueryBase(self.client)
 
     def request(
         self,
@@ -82,9 +79,9 @@ class MinimalHoneycombClient:
             variables['datapoint']['file']['data'] = filename
             response = self.client.execute(request_string, variables, files)
         else:
-            response = self.query.query(request_string, variables)
+            response = self.client.execute(request_string, variables)
         try:
-            return_value = response.get(request_name)
+            return_value = response[request_name]
         except:
             raise ValueError('Received unexpected response from Honeycomb: {}'.format(response))
         return return_value
@@ -118,6 +115,116 @@ class MinimalHoneycombClient:
             {top_level_string: [
                 {second_level_string: return_object}
             ]}
+        ]
+        request_string = self.request_string_formatter(object)
+        return request_string
+
+    def compound_request(
+        self,
+        parent_request_type,
+        parent_request_name,
+        child_request_list
+    ):
+        request_string = self.compound_request_string(
+            parent_request_type,
+            parent_request_name,
+            child_request_list
+        )
+        variables = dict()
+        files = FileUpload()
+        for child_request_index, child_request in enumerate(child_request_list):
+            child_request_name = child_request['name']
+            child_arguments = child_request['arguments']
+            if child_arguments is not None:
+                child_variables = dict()
+                for child_argument_name, child_argument_info in child_arguments.items():
+                    # print(child_argument_name)
+                    # print(child_argument_info['value'])
+                    child_variables[child_argument_name] = child_argument_info['value']
+                if child_request_name == 'createDatapoint':
+                    # Prepare upload package
+                    filename = uuid4().hex
+                    # print(child_variables)
+                    try:
+                        data = child_variables.get('datapoint').get('file').get('data')
+                    except:
+                        raise ValueError('createDatapoint arguments do not contain datapoint.file.data field')
+                    try:
+                        content_type = child_variables.get('datapoint').get('file').get('contentType')
+                    except:
+                        raise ValueError('createDatapoint arguments do not contain datapoint.file.contentType field')
+                    data_json = json.dumps(data)
+                    files.add_file(
+                        'variables.datapoint_{}.file.data'.format(child_request_index),
+                        filename,
+                        data_json,
+                        content_type
+                    )
+                    # Replace data with filename
+                    child_variables['datapoint']['file']['data'] = filename
+                for child_variable_name, child_variable_value in child_variables.items():
+                    variables['{}_{}'.format(child_variable_name, child_request_index)] = child_variable_value
+        response = self.client.execute(request_string, variables, files)
+        try:
+            return_value = response
+        except:
+            raise ValueError('Received unexpected response from Honeycomb: {}'.format(response))
+        return return_value
+
+    def compound_request_string(
+        self,
+        parent_request_type,
+        parent_request_name,
+        child_request_list
+    ):
+        num_child_requests = len(child_request_list)
+        top_level_argument_string_list = []
+        child_string_list = []
+        for child_request_index, child_request in enumerate(child_request_list):
+            child_request_name = child_request['name']
+            child_arguments = child_request['arguments']
+            child_return_object_name = child_request['return_object_name']
+            child_return_object = child_request['return_object']
+            if child_arguments is not None:
+                child_argument_string_list=[]
+                for argument_name, argument_info in child_arguments.items():
+                    top_level_argument_string_list.append('${}_{}: {}'.format(
+                        argument_name,
+                        child_request_index,
+                        argument_info['type']
+                    ))
+                    child_argument_string_list.append('{}: ${}_{}'.format(
+                        argument_name,
+                        argument_name,
+                        child_request_index
+                    ))
+                child_argument_list_string = ', '.join(child_argument_string_list)
+                child_request['child_string'] = '{}_{}: {}({})'.format(
+                    child_return_object_name,
+                    child_request_index,
+                    child_request_name,
+                    child_argument_list_string
+                )
+            else:
+                child_request['child_string'] = '{}_{}: {}'.format(
+                    child_return_object_name,
+                    child_request_index,
+                    child_request_name
+                )
+            if len(top_level_argument_string_list) > 0:
+                top_level_argument_list_string = ', '.join(top_level_argument_string_list)
+                top_level_string = '{} {}({})'.format(
+                    parent_request_type,
+                    parent_request_name,
+                    top_level_argument_list_string
+                )
+            else:
+                top_level_string = '{} {}'.format(
+                    parent_request_type,
+                    parent_request_name
+                )
+        object = [
+            {top_level_string: [{child_request['child_string']: child_request['return_object']} for child_request in child_request_list]}
         ]
         request_string = self.request_string_formatter(object)
         return request_string
